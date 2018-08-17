8dac5f00ff03ac847d58fc90e5d38b6612512f0c
musi <957028613@qq.com>
2018-08-18 01:15:03 +0800

    增加主部分的功能

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 !function(){
+    var EventCenter = {
+        on: function(type, handler){
+            $(document).on(type, handler)
+        },
+        fire: function(type, data){
+            $(document).trigger(type, data)
+        }
+    }
+
     var footer = {
         itemWidth: null,
         ulWidth: null,
@@ -22,7 +31,8 @@
             var html = ''
             channels.forEach(channel => {
                 html = html + `
-                <li data-channel-id='${channel.channel_id}'>
+                <li data-channel-id='${channel.channel_id}' 
+                data-channel-name='${channel.name}'>
                     <div class="cover"  
                     style="background-image:url(${channel.cover_small})"></div>
                     <h3>${channel.name}</h3>
@@ -80,9 +90,69 @@
             })
             this.$footer.on('click', 'li', function(){
                 $(this).addClass('active')
-                  .siblings().removeClass('active')           
-              })
+                  .siblings().removeClass('active')
+                EventCenter.fire('select-albumn',{
+                    cannelId: $(this).attr('data-channel-id'),
+                    channelName: $(this).attr('data-channel-name')  
+                })          
+            })
+        },  
+    }
+
+    var fm = {
+        channel_id: '',
+        channelName: '随机播放',
+        init: function() {
+            this.view = $('main')
+            this.audio = new Audio()
+            this.audio.autoplay = true
+
+            this.render()
+            this.bind()
+        },
+        render: function() {
+            this.loadMusic()
+        },
+        bind: function() {
+            EventCenter.on('select-albumn', (e, channelObj) => {
+                this.cannelId = channelObj.cannelId
+                this.channelName = channelObj.channelName
+                console.log(this.cannelId, this.channelName)
+                this.loadMusic()
+            })
+            $('.btn-play').on('click', () => {
+                $('.btn-play').addClass('none')
+                $('.btn-pause').removeClass('none')
+                this.audio.play()
+            })
+            $('.btn-pause').on('click', () => {
+                this.audio.pause()
+                $('.btn-pause') .addClass('none')
+                $('.btn-play').removeClass('none')
+            })
+            $('.btn-next').on('click', () => {
+                this.loadMusic()
+            })
+        },
+        loadMusic: function() {
+            var url = 'http://api.jirengu.com/fm/getSong.php?channel=' + this.cannelId
+            $.getJSON(url).done((song) =>{
+                this.song = song.song[0]
+                this.setMusic()
+            })
+        },
+        setMusic: function() {
+            this.audio.src = this.song.url
+            $('.bg-picture').css('background-image', 'url(' + this.song.picture + ')')
+            this.view.find('.aside figure').css('background-image', 'url(' + this.song.picture + ')')
+            this.view.find('.detail h1').text(this.song.title)
+            this.view.find('.detail .author').text(this.song.artist)
+            this.view.find('.detail .tag').text(this.channelName)
+            this.view.find('.btn-play').addClass('none')
+            this.view.find('.btn-pause').removeClass('none')
         },
     }
+
     footer.init()
+    fm.init()
 }()
