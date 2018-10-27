25cb1e2d66cb0dd933c7519d29acaef81d1462d7
musi <957028613@qq.com>
2018-10-27 23:48:30 +0800

    修改部分bug

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-!function(){
+! function () {
     var EventCenter = {
-        on: function(type, handler){
+        on: function (type, handler) {
             $(document).on(type, handler)
         },
-        fire: function(type, data){
+        fire: function (type, data) {
             $(document).trigger(type, data)
         }
     }
@@ -12,23 +12,23 @@
         itemWidth: null,
         ulWidth: null,
         ulPosition: null,
-        init: function() {
+        init: function () {
             this.$footer = $('footer')
             this.$box = $('.box')
-            this.$ul = this.$footer.find('ul') 
+            this.$ul = this.$footer.find('ul')
             this.canRight = true
             this.canLeft = false
             this.render()
             this.bind()
-            
+
         },
-        render: function() {
+        render: function () {
             $.getJSON('https://jirenguapi.applinzi.com/fm/getChannels.php')
                 .done((e) => {
                     this.renderFooter(e.channels)
                 })
         },
-        renderFooter: function(channels) {
+        renderFooter: function (channels) {
             var html = ''
             channels.forEach(channel => {
                 html = html + `
@@ -38,70 +38,70 @@
                     style="background-image:url(${channel.cover_small})"></div>
                     <h3>${channel.name}</h3>
                 </li>  
-                `   
+                `
             })
             this.$ul.append(html)
-            this.itemWidth = this.$ul.find('li').outerWidth(true)  
-            this.setStyle()   
+            this.itemWidth = this.$ul.find('li').outerWidth(true)
+            this.setStyle()
             this.ulWidth = parseFloat(this.$ul.css('width'))
             this.ulPosition = parseFloat(this.$ul.css('left'))
         },
-        setStyle: function() {
+        setStyle: function () {
             var number = this.$ul.find('li').length
             this.$ul.css({
-                width: number * this.itemWidth 
-            }) 
+                width: number * this.itemWidth
+            })
         },
-        bind: function() {
+        bind: function () {
             $('.right').on('click', () => {
                 if (this.canRight) {
                     this.canRight = false
-                    this.$ul.animate({  
+                    this.$ul.animate({
                         left: '-=' + 3 * this.itemWidth
-                    },400,() => {
-                        this.ulPosition = parseFloat(this.$ul.css('left')) 
+                    }, 400, () => {
+                        this.ulPosition = parseFloat(this.$ul.css('left'))
                         this.canLeft = true
-                        if(this.$box.width()-this.ulWidth >= this.ulPosition) {
+                        if (this.$box.width() - this.ulWidth >= this.ulPosition) {
                             this.canRight = false
                         } else {
                             this.canRight = true
                         }
                     })
-                }                  
+                }
             })
-            $('.left').on('click', () => { 
-                if(this.canLeft) {
+            $('.left').on('click', () => {
+                if (this.canLeft) {
                     this.canLeft = false
                     this.$ul.animate({
                         left: '+=' + 3 * this.itemWidth
-                    },400,() => {
-                        this.ulPosition = parseFloat(this.$ul.css('left')) 
-                        this.canRight = true  
-                        if(this.ulPosition >= -1) {
+                    }, 400, () => {
+                        this.ulPosition = parseFloat(this.$ul.css('left'))
+                        this.canRight = true
+                        if (this.ulPosition >= -1) {
                             this.canLeft = false
                         } else {
                             this.canLeft = true
                         }
                     })
                 }
-                
+
             })
-            this.$footer.on('click', 'li', function(){
+            this.$footer.on('click', 'li', function () {
                 $(this).addClass('active')
-                  .siblings().removeClass('active')
-                EventCenter.fire('select-albumn',{
+                    .siblings().removeClass('active')
+                EventCenter.fire('select-albumn', {
                     channelId: $(this).attr('data-channel-id'),
-                    channelName: $(this).attr('data-channel-name')  
-                })          
+                    channelName: $(this).attr('data-channel-name')
+                })
             })
-        },  
+        },
     }
 
     var fm = {
         loves: {},
         channelId: '',
         channelName: '随机播放',
-        init: function() {
+        init: function () {
             this.view = $('main')
             this.audio = new Audio()
             this.audio.autoplay = true
@@ -109,17 +109,15 @@
             if (this.loves !== null && !$.isEmptyObject(this.loves)) {
                 this.channelId = 'mylove'
                 this.channelName = 'MyLove'
-                $('.btn-heart').addClass('like')    
-            }    
+                $('.btn-heart').addClass('like')
+            }
             this.bind()
-            this.render() 
-            $('btn-pause').click()
-            $('btn-play').click() 
+            this.render()
         },
-        render: function() {
-            this.loadMusic()                                    
+        render: function () {
+            this.loadMusic()
         },
-        bind: function() {
+        bind: function () {
             var _this = this
             EventCenter.on('select-albumn', (e, channelObj) => {
                 if (channelObj.channelId === 'mylove') {
@@ -128,16 +126,17 @@
                         this.channelName = channelObj.channelName
                         $('.btn-heart').addClass('like')
                         this.loadMusic()
-                    }  
+                    }
                 } else {
                     this.channelId = channelObj.channelId
                     this.channelName = channelObj.channelName
                     $('.btn-heart').removeClass('like')
                     this.loadMusic()
-                }  
+                    $('.btn-play').addClass('none')
+                    $('.btn-pause').removeClass('none')
+                }
             })
             $('.btn-heart').on('click', () => {
-                console.log(this.song)
                 if (this.loves === null) {
                     this.loves = {}
                 }
@@ -147,7 +146,7 @@
                     window.localStorage.setItem('Loves', data)
                 } else {
                     this.loves[this.song.sid] = this.song
-                    var data = JSON.stringify(this.loves)     
+                    var data = JSON.stringify(this.loves)
                     window.localStorage.setItem('Loves', data)
                 }
                 $('.btn-heart').toggleClass('like')
@@ -165,13 +164,13 @@
             $('.btn-next').on('click', () => {
                 this.loadMusic()
             })
-            $('.bar').on('click', function(e){
+            $('.bar').on('click', function (e) {
                 var barLeft = $('.bar').offset().left
                 var clientX = e.clientX
                 var barWidth = this.offsetWidth
                 var newPosition = (clientX - barLeft) / barWidth * 100
-                $('.bar-progress').css('width', newPosition +'%')
-                _this.audio.currentTime = newPosition /100 * _this.audio.duration
+                $('.bar-progress').css('width', newPosition + '%')
+                _this.audio.currentTime = newPosition / 100 * _this.audio.duration
                 _this.updateStatus()
             })
 
@@ -180,16 +179,16 @@
                 clearInterval(this.statusClock)
                 this.statusClock = setInterval(() => {
                     this.updateStatus()
-                },1000)
-            })    
+                }, 1000)
+            })
             this.audio.addEventListener('pause', () => {
                 clearInterval(this.statusClock)
             })
             this.audio.addEventListener('ended', () => {
-                this.loadMusic()    
+                this.loadMusic()
             })
         },
-        loadMusic: function() {
+        loadMusic: function () {
             if (this.channelId === 'mylove') {
                 var song = []
                 for (let key in this.loves) {
@@ -197,63 +196,63 @@
                         song.push(this.loves[key])
                     }
                 }
-                this.index = Math.floor(Math.random()*(song.length))
+                this.index = Math.floor(Math.random() * (song.length))
                 this.song = song[this.index]
                 $('.btn-heart').addClass('like')
                 this.setMusic()
                 this.loadLyric()
             } else {
-                var url = 'https://jirenguapi.applinzi.com/fm/getSong.php?channel=' + this.cannelId
+                var url = 'https://jirenguapi.applinzi.com/fm/getSong.php?channel=' + this.channelId
                 $.getJSON(url)
-                    .done((song) =>{
-                        this.song = song.song[0]
-                        this.setMusic()
-                        this.loadLyric()  
+                    .done((song) => {
+                        if(!song.url){
+                            this.song = song.song[0]
+                            this.setMusic()
+                            this.loadLyric()
+                        }
                     })
-            }       
+            }
         },
-        setMusic: function() {
+        setMusic: function () {
             this.audio.src = this.song.url
             $('.bg-picture').css('background-image', 'url(' + this.song.picture + ')')
             this.view.find('.aside figure').css('background-image', 'url(' + this.song.picture + ')')
             this.view.find('.detail h1').text(this.song.title)
             this.view.find('.detail .author').text(this.song.artist)
             this.view.find('.detail .tag').text(this.channelName)
-            this.view.find('.btn-play').addClass('none')
-            this.view.find('.btn-pause').removeClass('none')
-            this.audio.autoplay = true
         },
-        updateStatus: function() {
-            var min = Math.floor(this.audio.currentTime/60)
-            var second = Math.floor(this.audio.currentTime%60) + ''
-            second = second.length === 2 ? second : '0' + second    
+        updateStatus() {
+            var min = Math.floor(this.audio.currentTime / 60) + ''
+            var second = Math.floor(this.audio.currentTime % 60) + ''
+            second = second.length === 2 ? second : '0' + second
             this.view.find('.current-time').text(min + ':' + second)
             this.view.find('.bar-progress').css(
-                'width', this.audio.currentTime/this.audio.duration*100+'%'
+                'width', this.audio.currentTime / this.audio.duration * 100 + '%'
             )
-            
-            var showingLyric = this.lyricObj['0'+ min + ':' + second] 
+            var showingLyric = this.lyricObj['0' + min + ':' + second]
             if (showingLyric) {
                 this.view.find('.lyric > p').text(showingLyric)
             }
         },
-        loadLyric: function() {
+        loadLyric() {
             var url = 'https://jirenguapi.applinzi.com/fm/getLyric.php?&sid=' + this.song.sid
-            $.getJSON(url).done((lyric) =>{
+            $.getJSON(url).done((lyric) => {
+                console.log('2222')
                 var lyric = lyric.lyric
+                console.log(lyric)
                 var lyricObj = {}
-                lyric.split('\n').forEach(function(line) {
+                lyric.split('\n').forEach(function (line) {
                     var times = line.match(/\d{2}:\d{2}/g)
                     var str = line.replace(/\[.+?\]/g, '')
                     if (Array.isArray(times)) {
-                        times.forEach(function(time){
+                        times.forEach(function (time) {
                             lyricObj[time] = str
                         })
                     }
                 })
                 this.lyricObj = lyricObj
             })
-        }, 
+        },
     }
     footer.init()
     fm.init()
